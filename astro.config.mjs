6ca5003d82551c09b7df56a8c9426359630a7ed9
Hex <maxwellturtle@icloud.com>
2024-07-12 09:47:00 -0400

    Initial commit with styles and action components.

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		{
+			name: "astro-material-design",
+			hooks: {
+				"astro:config:setup": (options) => {}
+			}
+		}
+	]
+});
